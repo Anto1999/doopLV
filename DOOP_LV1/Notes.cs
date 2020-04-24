09401088e6cd8824f17086bf56817c2bac5a148a
Anto Ivic <anto.ivic23@gmail.com>
2020-04-24 15:44:39 +0200

    zadatak3

--- DOOP_LV1/Notes.cs
+++ DOOP_LV1/Notes.cs
@@ -7,7 +7,6 @@ namespace DOOP_LV1
         private String text;
         private String author;
         private int pertinence;
-
         public string Text
         {
             get { return this.text; }
@@ -23,6 +22,55 @@ namespace DOOP_LV1
             get { return this.pertinence; }
             set { this.pertinence = value; }
         }
+        public string getText(){
+            return this.text;
+        }
+        public string getAuthor() {
+            return this.Author;
+        }
+        public int getPertinence()
+        {
+            return this.pertinence;
+        }
+        public void setName(string text){
+            this.text = text;
+        }
+       
+        public void setPertinence(int pertinence)
+        {
+            this.pertinence = pertinence;
+        }
+     
+
+        public Notes()
+        {
+            this.text = string.Empty;
+            this.author = string.Empty;
+            this.pertinence = 0;
+        }
+
+
+        public Notes(string text,string author, int pertinence)
+        {
+            this.text = text;
+            this.author = author;
+            this.pertinence = pertinence;
+
+        }
+
+        public Notes(string text,string author)
+        {
+            this.text = text;
+            this.author = author;
+
+        }
+        public override string ToString()
+        {
+            return this.text + this.author + this.pertinence;
+           
+        }
+
+
 
     }
 }
--- DOOP_LV1/Program.cs
+++ DOOP_LV1/Program.cs
@@ -6,7 +6,16 @@ namespace DOOP_LV1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+
+            Notes  notes = new Notes();           Notes notes1 = new Notes("Biljeska", "Ivan",2);
+            Notes notes2 = new Notes("biljeska2", "Ivo");
+            notes2.setPertinence(Convert.ToInt32(Console.ReadLine()));
+            Console.WriteLine(notes.ToString());
+            Console.WriteLine(notes1.ToString());
+            Console.WriteLine(notes2.ToString());
+
+
         }
     }
 }
