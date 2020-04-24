1e7de3ba4d8cdd9ba510a7a787bbf75563129b54
Anto Ivic <anto.ivic23@gmail.com>
2020-04-24 12:24:23 +0200

    Inital commit

---
+++ DOOP_LV1/Notes.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace DOOP_LV1
+{
+    class Notes
+    {
+        private String text;
+        private String author;
+        private int pertinence;
+
+        public string Text
+        {
+            get { return this.text; }
+            set { this.text = value; }
+        }
+        public string Author
+        {
+            get { return this.author; }
+            set { this.author = value; }
+        }
+        public int Pertinence
+        {
+            get { return this.pertinence; }
+            set { this.pertinence = value; }
+        }
+
+    }
+}
---
+++ DOOP_LV1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DOOP_LV1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
